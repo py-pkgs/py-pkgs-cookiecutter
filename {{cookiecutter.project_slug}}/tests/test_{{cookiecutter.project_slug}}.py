58ba28616058e713fb7392217c892907997d52a5
Tiffany A. Timbers <tiffany.timbers@stat.ubc.ca>
2021-03-10 22:11:41 -0800

    (fix): removing version from tests
    Having the package version in the tests file breaks semver tooling.

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,5 +1,2 @@
 from {{ cookiecutter.project_slug }} import __version__
 from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
-
-def test_version():
-    assert __version__ == {{cookiecutter.version}}
