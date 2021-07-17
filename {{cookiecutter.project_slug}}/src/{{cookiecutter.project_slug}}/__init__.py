d2fc6e3881d0d67ece19dfe8c2edd0a7172b123c
Tomas Beuzen <tomas.beuzen@stat.ubc.ca>
2021-07-17 17:44:40 +1000

    update __version__ defintion in __init__.py

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,1 +1,5 @@
-__version__ = "{{ cookiecutter.version }}"
+# importlib is included in the standard library for Python >= 3.8
+# here __version__ is obtained from installed version of package
+from importlib.metadata import version
+
+__version__ = version(__name__)
