809d5a49c1bff28ed28f5ab4fdcf56e4600608d1
Jasper Ginn <jasperginn@gmail.com>
2022-09-19 22:33:34 +0200

    feat: add skeleton CLI

--- {{ cookiecutter.__package_slug }}/src/{{ cookiecutter.__package_slug }}/cli.py
+++ {{ cookiecutter.__package_slug }}/src/{{ cookiecutter.__package_slug }}/cli.py
@@ -2,8 +2,15 @@ import logging
 import click
 
 logger = logging.getLogger("{{ cookiecutter.__package_slug }}.cli")
+handler = logging.StreamHandler()
+format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(format)
+logger.addHandler(handler)
 
 
 @click.command()
-def pipeline():
+@click.argument('input_file')
+def pipeline(input_file):
+    logger.info("Starting pipeline ...")
+    logger.debug(f"Ingesting file {input_file}")
     ...
