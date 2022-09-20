dafffec3ebcb00b14bbf609d20ef89094b4ebc8a
Jasper Ginn <jasperginn@gmail.com>
2022-09-20 08:43:43 +0200

    add nchar to pipeline

--- {{ cookiecutter.__package_slug }}/src/{{ cookiecutter.__package_slug }}/cli.py
+++ {{ cookiecutter.__package_slug }}/src/{{ cookiecutter.__package_slug }}/cli.py
@@ -2,7 +2,10 @@ import logging
 
 import click
 
+from .steps.example import nchar
+
 logger = logging.getLogger("{{ cookiecutter.__package_slug }}.cli")
+logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(format)
@@ -14,4 +17,5 @@ logger.addHandler(handler)
 def pipeline(input_file):
     logger.info("Starting pipeline ...")
     logger.debug(f"Ingesting file {input_file}")
-    ...
+    result = nchar(input_file)
+    logger.debug(f"File path has {result} characters")
