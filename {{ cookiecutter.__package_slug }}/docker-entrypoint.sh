4be42427bdcca138689c379662f6d5cb0d4b1fd6
MountainGod2 <admin@reid.ca>
2024-11-06 17:45:28 -0700

    refactor: Update docker-entrypoint.sh to improve command execution

--- {{ cookiecutter.__package_slug }}/docker-entrypoint.sh
+++ {{ cookiecutter.__package_slug }}/docker-entrypoint.sh
@@ -3,11 +3,13 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# If the first argument is a flag (starts with '-') or is empty, assume the default command is `chaturbate_poller`
+# If the first argument is a flag (starts with '-') or is empty, assume the default command is `{{ cookiecutter.__package_slug }}`
 if [ "${1#-}" != "$1" ] || [ -z "$1" ]; then
-  set -- python3 -m {{ cookiecutter.__package_slug }} "$@"
+  set -- python3 -m {{ cookiecutter.__package_slug }} "$@"  # Modify this line to match the command to start your application
 fi
 
 # Print the command to the console and execute it with the arguments passed to the entrypoint
 echo "Running command:" "$@"
+
+# Execute the command
 exec "$@"
