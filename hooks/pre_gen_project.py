95a03b1d20b7ba2e5c8997d75c6bbef34a388b60
Tiffany A. Timbers <tiffany.timbers@stat.ubc.ca>
2025-01-03 10:24:17 -0800

    fix indentation error

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -13,7 +13,7 @@ except PackageNotFoundError:
 
 min_version = version.parse(MIN_CC_VERSION)
 if cc_version is None or cc_version < min_version:
-        print(
+    print(
         f"ERROR: Please install cookiecutter >= {MIN_CC_VERSION} "
         f"(current version is {cc_version if cc_version else 'not installed'}):\n"
         f"\tpip install 'cookiecutter>=2', OR\n"
