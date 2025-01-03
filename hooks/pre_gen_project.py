fbd138f5ac0ee9e452429821b4b1935526ac9d26
Tiffany A. Timbers <tiffany.timbers@stat.ubc.ca>
2025-01-03 10:16:51 -0800

    Update pre_gen_project.py to work with Python 3.12

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,19 +1,21 @@
 import sys
 
-from pkg_resources import (
-    get_distribution,
-    packaging,
-)
+from importlib.metadata import distribution, PackageNotFoundError
+from packaging import version
 
 MIN_CC_VERSION = "2.0.0"
 
 # assert cookiecutter >= 2.0.0
-cc_version = packaging.version.parse(get_distribution("cookiecutter").version)
-min_version = packaging.version.parse(MIN_CC_VERSION)
-if cc_version < min_version:
-    print(
-        f"ERROR: please install cookiecutter >= {MIN_CC_VERSION} (current "
-        f"version is {cc_version}):\n"
+try:
+    cc_version = version.parse(distribution("cookiecutter").version)
+except PackageNotFoundError:
+    cc_version = None
+
+min_version = version.parse(MIN_CC_VERSION)
+if cc_version is None or cc_version < min_version:
+        print(
+        f"ERROR: Please install cookiecutter >= {MIN_CC_VERSION} "
+        f"(current version is {cc_version if cc_version else 'not installed'}):\n"
         f"\tpip install 'cookiecutter>=2', OR\n"
         f"\tconda install 'cookiecutter>=2'"
     )
