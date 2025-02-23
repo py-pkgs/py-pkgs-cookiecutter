26da0901f5438d4a23e72831edae0a2989466587
Tomas Beuzen <tomas.beuzen@gmail.com>
2025-02-23 20:38:40 +1100

    fix: remove packaging import

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,17 +1,22 @@
 import sys
-
-from importlib.metadata import distribution, PackageNotFoundError
-from packaging import version
+from importlib.metadata import (
+    PackageNotFoundError,
+    distribution,
+)
 
 MIN_CC_VERSION = "2.0.0"
 
+def version(v):
+    return tuple(map(int, (v.split("."))))
+
+
 # assert cookiecutter >= 2.0.0
 try:
-    cc_version = version.parse(distribution("cookiecutter").version)
+    cc_version = version(distribution("cookiecutter").version)
 except PackageNotFoundError:
     cc_version = None
 
-min_version = version.parse(MIN_CC_VERSION)
+min_version = version(MIN_CC_VERSION)
 if cc_version is None or cc_version < min_version:
     print(
         f"ERROR: Please install cookiecutter >= {MIN_CC_VERSION} "
